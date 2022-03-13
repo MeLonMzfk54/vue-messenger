f840f86b1102ffb8ca5cdb179ff475c2af04aa41
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-03-13 21:56:29 +0700

    clear project + skelet of project

---
+++ src/components/main-wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-wrapper',
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {}
+  }
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'material-design-icons-iconfont'
+
+import './assets/'
 
 createApp(App).use(store).use(router).mount('#app')
