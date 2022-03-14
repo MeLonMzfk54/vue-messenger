1b6969b6f9868020f7e1260f6cda2edd949afd8f
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-03-14 22:29:10 +0700

    contact detail page

--- src/components/contacts/contact-user-info.vue
+++ src/components/contacts/contact-user-info.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="contact-user-info"></div>
+  <div class="contact-user-info">
+    <div class="info__avatar"></div>
+    <div class="info__content">
+      <div class="info__phone">
+        <span>{{ contact.phone }}</span>
+      </div>
+      <div class="info__tools">
+        <button class="info__button start-call">Call</button>
+        <button class="info__button start-chat">Start Chat</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex'
+
 export default {
   name: 'contact-user-info',
   props: {},
   data() {
-    return {}
+    return {
+      contact: {}
+    }
   },
-  computed: {}
+  computed: {
+    ...mapState([
+        'contacts'
+    ])
+  },
+  mounted() {
+    this.contact = this.contacts.find((contact) => contact.id == this.$route.query.id)
+  }
 }
 </script>
 
